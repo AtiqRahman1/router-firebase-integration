3a18f5d9ddbc0c3368a583bcaa7a7ef9c1a43cf8
AtiqRahman1 <atiq94.ar@gmail.com>
2022-04-09 00:37:28 +0600

    initial setup of login and register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h3>Login page</h3>
+            <h3>Please Login</h3>
+            <form>
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Your password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
